bc7ddd014fd23cd30ae4d85fa685deec5b986422
Jiri Malek <jirka.malek@gmail.com>
2020-07-13 10:27:25 +0200

    Add ChainLink price oracles aggregator contract.

---
+++ contracts/ChainLinkPriceReferenceAggregator.sol
@@ -0,0 +1,101 @@
+pragma solidity ^0.5.0;
+
+// AggregatorInterface specifies ChainLink aggregator interface.
+interface AggregatorInterface {
+    function latestAnswer() external view returns (int256);
+
+    function latestTimestamp() external view returns (uint256);
+
+    function latestRound() external view returns (uint256);
+
+    function getAnswer(uint256 roundId) external view returns (int256);
+
+    function getTimestamp(uint256 roundId) external view returns (uint256);
+
+    event AnswerUpdated(int256 indexed current, uint256 indexed roundId, uint256 timestamp);
+    event NewRound(uint256 indexed roundId, address indexed startedBy, uint256 startedAt);
+}
+
+// ChainLinkPriceReferenceAggregator implements aggregation for set of ChainLink
+// reference price oracles redirecting price requests to their target price
+// oracle based on the denomination address
+contract ChainLinkPriceReferenceAggregator {
+    // owner represents the manager address of the oracle aggregator.
+    address public owner;
+
+    // ChainLink aggregator interface references by denomination.
+    // The left side is the token identifier, the right side
+    // is the ChainLink aggregator address.
+    mapping(address => AggregatorInterface) public aggregators;
+
+    // AggregatorChanged event is emitted when a new price oracle aggregator is set for a token.
+    event AggregatorChanged(address indexed token, address aggregator, uint timestamp);
+
+    // install new aggregator instance
+    constructor() public {
+        // remember the manager address
+        owner = msg.sender;
+    }
+
+    // setAggregator sets a new aggregator reference for the given token.
+    function setAggregator(address token, address aggregator) external {
+        // only owner can make the change
+        require(msg.sender == owner, "access restricted");
+
+        // make the change
+        aggregators[token] = AggregatorInterface(aggregator);
+
+        // emit notification to express the change
+        emit AggregatorChanged(token, aggregator, now);
+    }
+
+    // getPrice returns the latest price available for the token specified.
+    function getPrice(address token) public view returns (int256) {
+        // the price oracle must be set for the token
+        require(aggregators[token] != AggregatorInterface(0), "oracle not available");
+
+        // get the latest answer from the aggregator of the given token
+        return aggregators[token].latestAnswer();
+    }
+
+    // getTimeStamp returns the time stamp the latest price was made available
+    // by the ChainLink aggregator oracle for the token specified.
+    function getTimeStamp(address token) public view returns (uint256) {
+        // the price oracle must be set for the token
+        require(aggregators[token] != AggregatorInterface(0), "oracle not available");
+
+        // get the latest answer from the aggregator of the given token
+        return aggregators[token].latestTimestamp();
+    }
+
+    // getPreviousPrice returns the price available for the token specified
+    // from the ChainLink aggregator oracle <_back> update cycles before the most recent one.
+    function getPreviousPrice(address token, uint256 _back) public view returns (int256) {
+        // the price oracle must be set for the token
+        require(aggregators[token] != AggregatorInterface(0), "oracle not available");
+
+        // get the latest answer round from the aggregator of the given token
+        // we make sure the requested round is valid
+        uint256 latest = aggregators[token].latestRound();
+        require(_back <= latest, "not enough history");
+
+        // get the price back then
+        return aggregators[token].getAnswer(latest - _back);
+    }
+
+    // getTimeStamp returns the time stamp the price was made available
+    // by the ChainLink aggregator oracle <_back> update cycles before the most recent one.
+    function getPreviousTimeStamp(address token, uint256 _back) public view returns (uint256) {
+        // the price oracle must be set for the token
+        require(aggregators[token] != AggregatorInterface(0), "oracle not available");
+
+        // get the latest answer round from the aggregator of the given token
+        // we make sure the requested round is valid
+        uint256 latest = aggregators[token].latestRound();
+        require(_back <= latest, "not enough history");
+
+        // get the time stamp back then
+        return aggregators[token].getTimestamp(latest - _back);
+    }
+
+}
