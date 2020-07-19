a34b5a5d7be8c75606e4bef2d3b2a79ba53ac3fe
Jiri Malek <jirka.malek@gmail.com>
2020-07-19 15:21:12 +0200

    Pull token details from the ERC20 contract.
    Synthetic tokens are managed by ERC20 contracts which hold the token name, description and decimals configuration. We pull that information from the linked ERC20 contract where possible.

--- contracts/ChainLinkPriceReferenceAggregator.sol
+++ contracts/ChainLinkPriceReferenceAggregator.sol
@@ -3,18 +3,52 @@ pragma solidity ^0.5.0;
 // AggregatorInterface specifies ChainLink aggregator interface.
 import "./AggregatorInterface.sol";
 
+// IERC20Detailed defines the interface to ERC20Detailed token contract.
+// We use this type of contract, @openzeppelin implementation,
+// for our synthetic DeFi ERC20 tokens.
+interface IERC20Detailed {
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() external view returns (string memory);
+
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
+    function symbol() external view returns (string memory);
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5,05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei.
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() external view returns (uint8);
+}
+
 // ChainLinkPriceReferenceAggregator implements aggregation for set of ChainLink
 // reference price oracles redirecting price requests to their target price
 // oracle based on the denomination address.
 contract ChainLinkPriceReferenceAggregator {
+    // nativeToken represents the address of the native FTM tokens pool.
+    address public nativeToken = 0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF;
+
     // TokenInformation represents a single token handled by the reference aggregator.
     // The De-Fi API uses this reference to do on-chain tokens tracking.
     struct TokenInformation {
-        address addr;       // address of the token (unique identifier)
-        bytes32 name;       // Name fo the token
-        bytes32 symbol;     // symbol of the token
+        address token;      // address of the token (unique identifier)
+        string name;        // Name fo the token
+        string symbol;      // symbol of the token
         string logo;        // URL address of the token logo
-        uint decimals;      // number of decimals the token's price oracle uses
+        uint8 decimals;     // number of decimals the token itsdelf uses
+        uint8 priceDecimals;// number of decimals the token's price oracle uses
         bool isActive;      // is this token active in DeFi?
         bool canDeposit;    // is this token available for deposit?
         bool canBorrow;     // is this token available for fLend?
@@ -37,7 +71,7 @@ contract ChainLinkPriceReferenceAggregator {
     event AggregatorChanged(address indexed token, address aggregator, uint timestamp);
 
     // TokenInformationAdded event is emitted when a new token information is added to the contract.
-    event TokenInformationAdded(address indexed token, uint index, bytes32 name, uint timestamp);
+    event TokenInformationAdded(address indexed token, string name, uint index, uint timestamp);
 
     // TokenInformationUpdated event is emitted when an existing token information is updated.
     event TokenInformationChanged(address indexed token, uint index, uint timestamp);
@@ -131,10 +165,10 @@ contract ChainLinkPriceReferenceAggregator {
     }
 
     // findTokenIndex finds an index of a token in the tokens list by address; returns -1 if not found.
-    function findTokenIndex(address _addr) public view returns (int256) {
+    function findTokenIndex(address _token) public view returns (int256) {
         // loop the list and try to find the token
         for (uint256 i = 0; i < tokens.length; i++) {
-            if (tokens[i].addr == _addr) {
+            if (tokens[i].token == _token) {
                 return int256(i);
             }
         }
@@ -143,12 +177,10 @@ contract ChainLinkPriceReferenceAggregator {
 
     // addToken adds new token into the reference contract.
     function addToken(
-        address _addr,
+        address _token,
         address _aggregator,
-        bytes32 _name,
-        bytes32 _symbol,
         string calldata _logo,
-        uint _decimals,
+        uint8 _priceDecimals,
         bool _isActive,
         bool _canDeposit,
         bool _canBorrow,
@@ -159,19 +191,40 @@ contract ChainLinkPriceReferenceAggregator {
         require(msg.sender == owner, "access restricted");
 
         // try to find the address
-        require(0 > findTokenIndex(_addr), "token already known");
+        require(0 > findTokenIndex(_token), "token already known");
 
         // set the price aggregator
-        aggregators[_addr] = AggregatorInterface(_aggregator);
-        emit AggregatorChanged(_addr, _aggregator, now);
+        aggregators[_token] = AggregatorInterface(_aggregator);
+        emit AggregatorChanged(_token, _aggregator, now);
+
+        // declare needed token details (either native, or ERC20 synth)
+        string memory _name;
+        uint8 _decimals;
+        string memory _symbol;
+
+        // pull the symbol of the token
+        if (_token == nativeToken) {
+            _decimals = 18;
+            _name = "Native TFM Token";
+            _symbol = "FTM";
+        } else {
+            // pull decimals from the ERC20 token
+            _decimals = IERC20Detailed(_token).decimals();
+            require(_decimals > 0, "token decimals invalid");
+
+            // pull the name and symbol of the token
+            _name = IERC20Detailed(_token).name();
+            _symbol = IERC20Detailed(_token).symbol();
+        }
 
         // add the token to the list
         tokens.push(TokenInformation({
-            addr : _addr,
+            token : _token,
             name : _name,
             symbol : _symbol,
             logo: _logo,
             decimals : _decimals,
+            priceDecimals : _priceDecimals,
             isActive : _isActive,
             canDeposit : _canDeposit,
             canBorrow : _canBorrow,
@@ -181,14 +234,14 @@ contract ChainLinkPriceReferenceAggregator {
         );
 
         // inform
-        emit TokenInformationAdded(_addr, tokens.length - 1, _name, now);
+        emit TokenInformationAdded(_token, _name, tokens.length - 1, now);
     }
 
     // updateToken modifies existing token in the reference contract.
     function updateToken(
-        address _addr,
+        address _token,
         string calldata _logo,
-        uint _decimals,
+        uint8 _priceDecimals,
         bool _isActive,
         bool _canDeposit,
         bool _canBorrow,
@@ -199,12 +252,12 @@ contract ChainLinkPriceReferenceAggregator {
         require(msg.sender == owner, "access restricted");
 
         // try to find the address in the tokens list
-        int256 ix = findTokenIndex(_addr);
+        int256 ix = findTokenIndex(_token);
         require(0 <= ix, "token not known");
 
         // update token details in the contract
         tokens[uint256(ix)].logo = _logo;
-        tokens[uint256(ix)].decimals = _decimals;
+        tokens[uint256(ix)].priceDecimals = _priceDecimals;
         tokens[uint256(ix)].isActive = _isActive;
         tokens[uint256(ix)].canDeposit = _canDeposit;
         tokens[uint256(ix)].canBorrow = _canBorrow;
@@ -212,6 +265,6 @@ contract ChainLinkPriceReferenceAggregator {
         tokens[uint256(ix)].volatility = _volatility;
 
         // inform
-        emit TokenInformationChanged(_addr, uint256(ix), now);
+        emit TokenInformationChanged(_token, uint256(ix), now);
     }
 }
